73ab66276ced70061250ac58b44a65bde1b98689
zubaria <zubariaayub634@gmail.com>
2021-04-20 11:17:34 +0500

    Creates training workspace

---
+++ createWorkspace.py
@@ -0,0 +1,11 @@
+from os import mkdir
+
+mkdir("workspace")
+mkdir("workspace/training_demo")
+mkdir("workspace/training_demo/annotations")
+mkdir("workspace/training_demo/exported_models")
+mkdir("workspace/training_demo/images")
+mkdir("workspace/training_demo/images/test")
+mkdir("workspace/training_demo/images/train")
+mkdir("workspace/training_demo/models")
+mkdir("workspace/training_demo/pre-trained-models")
